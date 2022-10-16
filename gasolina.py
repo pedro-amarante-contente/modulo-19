1be2f4373b47dc4bcf2868dc0545cb056a7f89ff
seu-usuario-git <pedroacontente@gmail.com>
2022-10-16 03:15:23 +0000

    Atualização no código e gráfico

--- gasolina.py
+++ gasolina.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 gasolina_df = pd.read_csv('gasolina.csv', sep=',')
 with sns.axes_style('whitegrid'):
-  grafico = sns.lineplot(data=gasolina_df, x='dia', y='venda')
-  grafico.set(ylabel='preço médio')
-  grafico.figure.set_size_inches(w=30/2.54, h=15/2.54)
-  grafico.get_figure().savefig(f'gasolina.png')
+grafico = sns.lineplot(data=gasolina_df, x='dia', y='venda')
+grafico.set(title='Preço médio da gasolina por dia Jun/2021', ylabel='PREÇO', xlabel='DIA')
+grafico.figure.set_size_inches(w=30/2.54, h=15/2.54)
+grafico.get_figure().savefig(f'gasolina.png')
